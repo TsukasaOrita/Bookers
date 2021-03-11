70a8897fb220426acd7f14a3699a12970d5e70b9
TsukasaOrita <tsukasa680@gmail.com>
2021-03-11 14:52:52 +0000

    [finish]5-8

--- bookers/app/controllers/homes_controller.rb
+++ bookers/app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
+
   def top
-    @homes = Home.top
   end
+
 end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-
-  get 'homes/top'
+  root to: 'homes#top'
+  resources :homes
   resources :books
 
 end
